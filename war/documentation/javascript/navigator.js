8152e85f3ef5dae0aa854b7efb6b990062498cc9
mmale <mmale@web.de>
2009-08-29 16:31:05 +0100

    SOA4all m18  submission and FIS tutorial stable version

--- war/documentation/javascript/navigator.js
+++
@@ -1,7 +0,0 @@
-// show a navigator bar inside the page who use the "navigator" ID
-function showNavigator() {
-	var navigator = document.getElementById("navigator");
-	var code = "<a class='linkable' href='index.html'>Index</a> | " +
-			   "<a class='linkable' href='usecase.html'>&lt;-- Use case diagram</a><br /><br />";
-	navigator.innerHTML = code;
-}
